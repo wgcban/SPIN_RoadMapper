31093dad96478ef7665bc6931b89573424c709a3
wgcban <chaminda.bandara@eng.pdn.ac.lk>
2024-01-17 23:55:56 -0500

    added missing files: models

--- model/models.py
+++ model/models.py
@@ -1,8 +1,8 @@
 from linknet import LinkNet34, LinkNet34MTL
 from stack_module import StackHourglassNetMTL
-from SPIN import SPIN
-from unet import UNet
+from SPIN import spin
+from unet import unet
 
-MODELS = {"LinkNet34MTL": LinkNet34MTL, "StackHourglassNetMTL": StackHourglassNetMTL, "SPIN": SPIN, "UNet": UNet}
+MODELS = {"LinkNet34MTL": LinkNet34MTL, "StackHourglassNetMTL": StackHourglassNetMTL, "SPIN": spin, "UNet": unet}
 
 MODELS_REFINE = {"LinkNet34": LinkNet34}
